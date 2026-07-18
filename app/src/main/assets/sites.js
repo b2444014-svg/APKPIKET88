b764e71e2e2e556cbfc752dbdd1b33c7daf97848
b2444014-svg <b2444014@gmail.com>
2026-07-18 22:40:48 +0700

    Update sites.js

--- app/src/main/assets/sites.js
+++ app/src/main/assets/sites.js
@@ -3,12 +3,12 @@
    (kosongkan dengan "" jika mau pakai tampilan default)
    ======================================================= */
 
-window.LOGO_URL = "https://i.ibb.co/Y446WRjk/nabung77-LOGO-APK.jpg";
-window.BG_URL   = "https://i.ibb.co/Txx3QDGR/background-apk-nabung.jpg";
-window.APP_NAME = "NABUNG77";
+window.LOGO_URL = "https://i.ibb.co/G4tHqJNW/PIKET88-LOGO.jpg";
+window.BG_URL   = "https://i.ibb.co/B5xHcfYg/BG-PIKET88.jpg";
+window.APP_NAME = "PIKET88";
 
 /* Link Box Event & Pesan (live dari panel admin Worker) */
-window.INFO_URL = "https://playgame-notif.b2444014.workers.dev/info";
+window.INFO_URL = "https://piket88-notif.sandiworkingemail.workers.dev/info";
 
 
 /* =======================================================
@@ -22,17 +22,17 @@ window.INFO_URL = "https://playgame-notif.b2444014.workers.dev/info";
 
 window.SITES = [
   {
-    name: "NABUNG77",
+    name: "PIKET88",
     category: "SLOT",
-    image: "https://i.ibb.co/Y446WRjk/nabung77-LOGO-APK.jpg",
-    url: "https://cutt.ly/nabung77seobambu",
+    image: "https://i.ibb.co/RpjFr1Fn/53-9b68e047ed684295bbc19b05ed70da0b.png",
+    url: "https://cutt.ly/piket88okz",
     premium: true
   },
   {
-    name: "GARASIPOKER",
-    category: "PKV",
-    image: "https://i.ibb.co/gMdz90LD/logo-garasi-poker-di-dalam.jpg",
-    url: "https://cutt.ly/garasipoker-login"
+    name: "GGBET303",
+    category: "MPO",
+    image: "https://i.ibb.co/CKPxCpyX/Image-20260607140731-6-9.jpg",
+    url: "https://cutt.ly/pastiggbet"
   }
   // Tambahkan game lain di sini, pisahkan dengan koma ( , )
 ];
